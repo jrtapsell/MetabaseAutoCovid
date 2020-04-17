ccd22041a9a8f4c193dd58c3da1882918b0fc7e6
James Tapsell <email@jrtapsell.co.uk>
2020-04-17 10:17:57 +0100

    Fixed capitalisation on the name of the countries file

--- postgres/10-import.sql
+++ postgres/10-import.sql
@@ -7,7 +7,7 @@ COPY raw_latest(Province_State, Country_Region, Last_Update, Lat, Long_, Confirm
 COPY raw_countries(Name, Code) FROM '/tmp/data/countries.csv' DELIMITER ',' CSV HEADER;
 COPY ammend_countries(Name, Code) FROM '/app/countries_ammendments.csv' DELIMITER ',' CSV HEADER;
 
-COPY raw_nhs_contries(Area_Code,Area_Name,Date,Value) FROM '/tmp/nhs/Countries.csv' DELIMITER ',' CSV HEADER;
+COPY raw_nhs_contries(Area_Code,Area_Name,Date,Value) FROM '/tmp/nhs/countries.csv' DELIMITER ',' CSV HEADER;
 COPY raw_nhs_regions(Area_Code,Area_Name,Date,Value) FROM '/tmp/nhs/NHS Regions.csv' DELIMITER ',' CSV HEADER;
 COPY raw_nhs_recovered(Date,Cumulative_Counts) FROM '/tmp/nhs/Recovered patients.csv' DELIMITER ',' CSV HEADER;
 COPY raw_nhs_cases (Date,Cases,Cumulative_Cases) FROM '/tmp/nhs/UK Cases.csv' DELIMITER ',' CSV HEADER;
